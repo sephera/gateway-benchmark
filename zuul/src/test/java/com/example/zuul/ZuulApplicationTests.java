b8978edb2d5f5a8690d3ba16d61b4904af540bc8
Hai Nguyen <nduyhai@users.noreply.github.com>
2019-10-29 20:44:55 +0700

    add zuul

--- zuul/src/test/java/com/example/zuul/ZuulApplicationTests.java
+++ zuul/src/test/java/com/example/zuul/ZuulApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.zuul;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ZuulApplicationTests {
 
     @Test
     void contextLoads() {
