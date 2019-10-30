38c3e3a2ef85fd171c4b2a36b8832b8e3d9fb89e
Hai Nguyen <nduyhai@users.noreply.github.com>
2019-10-30 20:49:07 +0700

    fix script build

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-mvn clean package -DskipTests & \
+mvn clean package -DskipTests &&  \
 ./resource-server/server & \
 java -jar ./zuul/target/zuul-0.0.1-SNAPSHOT.jar & \
 java -jar ./gateway/target/gateway-0.0.1-SNAPSHOT.jar &
